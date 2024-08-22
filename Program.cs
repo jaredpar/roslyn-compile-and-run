72d6167de7fc6cdb9a700ea063797c31d6eb1767
Jared Parsons <jared@paranoidcoding.org>
2024-08-22 16:14:12 -0700

    Quick sample

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+using System.Text;
+using Basic.Reference.Assemblies;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+
+var count = 0;
+var text = new StringBuilder();
+while (true)
+{
+    Console.WriteLine("Enter C# code type 'end' to finish: ");
+    while (Console.ReadLine() is var line && line != "end")
+    {
+        text.AppendLine(line);
+    }
+
+    var assemblyName = $"assembly{count++}";
+    try
+    {
+        Console.Write("Compiling ... ");
+        var stream = Compile(text.ToString(), assemblyName);
+        Console.WriteLine("Runnning ... ");
+        LoadAndRun(stream, assemblyName);
+        Console.WriteLine();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine();
+        Console.WriteLine(e.Message);
+        Console.WriteLine();
+    }
+}
+
+Stream Compile(string code, string assemblyName)
+{
+    var fullCode = $$"""
+        using System;
+        using System.Collections.Generic;
+
+        class Lib
+        {
+            static void Go()
+            {
+                {{code}}
+            }
+        }
+        """;
+
+    var options = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary);
+    var compilation = CSharpCompilation.Create(
+        assemblyName,
+        [CSharpSyntaxTree.ParseText(fullCode)],
+        Net80.References.All,
+        options);
+
+    var peStream = new MemoryStream();
+    var emitResult = compilation.Emit(peStream);
+    if (!emitResult.Success)
+    {
+        throw new Exception(GetMessage(emitResult.Diagnostics));
+    }
+
+    peStream.Position = 0;
+    return peStream;
+}
+
+void LoadAndRun(Stream stream, string assemblyName)
+{
+    var alc = new AssemblyLoadContext(assemblyName);
+    var assembly = alc.LoadFromStream(stream);
+    var libType = assembly
+        .GetTypes()
+        .Where(x => x.Name == "Lib")
+        .Single();
+    var method = libType.GetMethod("Go", BindingFlags.Static | BindingFlags.NonPublic);
+    method!.Invoke(null, null);
+}
+
+string GetMessage(IEnumerable<Diagnostic> diagnostics)
+{
+    var builder = new StringBuilder();
+    builder.AppendLine("Compilation failed with the following errors:");
+    foreach (var d in diagnostics)
+    {
+        builder.AppendLine(d.ToString());
+    }
+    return builder.ToString();
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# roslyn-compile-and-run
-Sample for compiling, loading and executing code
+# Compile, Load and Run
+
+This is a sample application demonstrating how you can compile C# code on the fly, load it into an `AssemblyLoadContext` and execute it.
+
+For example: 
+
+```cmd
+> dotnet run
+Enter C# code type 'end' to finish: 
+Console.WriteLine("Hello World");
+end
+Compiling ... Runnning ... 
+Hello World
+
+Enter C# code type 'end' to finish:
+Console.WriteLine(DateTime.Now);
+end
+Compiling ... Runnning ... 
+Hello World
+8/22/2024 4:13:49 PM
+
+Enter C# code type 'end' to finish:
+```
